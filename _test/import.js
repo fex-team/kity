3abc36f0397ed7309c0e1c67a9e06b0ef5d60c08
yancend <dongyancen@baidu.com>
2013-10-12 19:18:40 +0800

    [test]测试框架

---
+++ _test/import.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: dongyancen
+ * Date: 13-10-10
+ * Time: 上午1:54
+ * To change this template use File | Settings | File Templates.
+ */
+///import core/kity;
+
