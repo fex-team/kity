165d867779b1fbda25011bb886eeb95f1c4bdfd7
techird <techurd@qq.com>
2013-10-16 14:00:26 +0800

    graphic specification framework

---
+++ spec/graphic/image.js
@@ -0,0 +1,29 @@
+describe("Kity.Image", function() {
+    it("should be an instance of Shape", function() {
+
+    });
+
+    describe("Image(url)", function() {
+        it("should create an image with initial url", function() {
+
+        });
+    });
+
+    describe("setUrl()", function() {
+        it("should set url of the image", function() {
+
+        });
+
+        it("should return this reference", function() {
+
+        });
+    });
+
+    describe("getUrl()", function() {
+        it("should return url of the image", function() {
+
+        })
+    });
+
+
+});
---
+++ spec/graphic/ployline.js
@@ -0,0 +1,31 @@
+describe("Kity.Polyline", function () {
+    it("should be an instance of Path", function() {
+
+    });
+
+    it("should be an instance of Shape", function() {
+
+    });
+
+    it("should extend Parent", function() {
+
+    });
+
+    describe("Polyline(points)", function() {
+        it("should create an polyline with initial points", function() {
+
+        });
+    });
+
+    describe("addChild(point)", function() {
+        it("should add a key point to the polyline", function() {
+
+        });
+    });
+
+    describe("removeChild(pos)", function() {
+        it("should remove a key point from the polyline in given position", function() {
+
+        });
+    });
+});
