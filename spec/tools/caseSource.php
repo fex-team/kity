fe8a3c4e553ce940afc3bfb8794cf61ad6245b6b
yancend <dongyancen@baidu.com>
2013-10-28 11:40:59 +0800

    [test]引入sea

--- spec/tools/caseSource.php
+++ spec/tools/caseSource.php
@@ -32,6 +32,13 @@ class Kiss
     public function print_js(  )
     {
 
+        print "<script type='text/javascript' src='../../dev-lib/sea-debug.js' ></script>\n";
+        print "<script>seajs.config( {
+            base: '../../src'
+
+        } );
+        seajs.use( 'main' );
+        </script>\n";
         /*load ua*/
         print "<script type='text/javascript' src='./js/UserAction.js' ></script>\n";
         /* load case source*/
@@ -42,10 +49,8 @@ class Kiss
         $ps = explode( '/' , $this->name );
         /*load helper*/
         foreach(Config::$helperFiles as $f){
-            array_pop( $ps );
-            array_push( $ps , $f );
-            if ( file_exists( $this->testPath . implode( '/' , $ps ) ) ) {
-                print '<script type="text/javascript" src="' . $this->testPath .  implode( '/' , $ps ) . '"></script>' . "\n";
+            if ( file_exists( $this->testPath .  $f  ) ) {
+                print '<script type="text/javascript" src="' . $this->testPath . $f  . '"></script>' . "\n";
             }
         }
         print '<script type="text/javascript" src="' . $this->testPath. $this->name . '.js"></script>' . "\n";
