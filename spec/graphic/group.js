9119df12d197d2d8f6c37bb3bc36c4e3329a539c
techird <techird@qq.com>
2013-10-31 14:42:35 +0800

    matrix

--- spec/graphic/group.js
+++ spec/graphic/group.js
@@ -1,9 +1,18 @@
+var Group = require('graphic/group');
+var Parent = require('graphic')
+
 describe("Kity.Group", function () {
+    var group;
+
+    beforeEach(function() {
+        group = new Group();
+    });
+
 	it("should be an instance of Shape", function() {
-		
+		expect(group instanceof Group).toBeTruthy();
 	});
 
 	it("should extend parent", function() {
-		
+		expect(group).toImplement(Parent);
 	});
 });
---
+++ src/graphic/matrix.js
@@ -0,0 +1,19 @@
+define(function(require, exports, module) {
+    var utils = require('core/utils');
+    require('core/class').createClass("graphic.Matrix", {
+        constructor: function() {
+            this.setMatrix(1, 0, 0, 1, 0, 0);
+        },
+        setMatrix: function(a, b, c, d, e, f) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+            this.f = f;
+        },
+        analyseSimple: function() {
+            
+        }
+    });
+});
