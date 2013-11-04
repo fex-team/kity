98bf424b262df51af619efd8769cf51cbf66705f
techird <techird@qq.com>
2013-11-04 17:48:24 +0800

    dev

--- src/graphic/color.js
+++ src/graphic/color.js
@@ -1,4 +1,5 @@
 define(function(require, exports, module) {
+    // 如果冲突删除本段，这是个桩
     var Color = require('core/class').createClass( 'Color', {
         constructor: function(value) {
             this.value = value;
