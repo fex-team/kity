c47e00aec1760837e6400477932d9496bda79827
techird <techird@qq.com>
2013-11-29 11:06:30 +0800

    Animate 基础接口
    快捷接口还在开发

---
+++ src/animate/animator.js
@@ -0,0 +1,100 @@
+define( function ( require, exports, module ) {
+
+    function parseTime( str ) {
+        var value = parseFloat( str, 10 );
+        if ( /ms/.test( str ) ) {
+            return value;
+        }
+        if ( /s/.test( str ) ) {
+            return value * 1000;
+        }
+        if ( /min/.test( str ) ) {
+            return value * 60 * 1000;
+        }
+        return value;
+    }
+
+    var Timeline = require( 'animate/timeline' );
+
+    var Animator = require( 'core/class' ).createClass( 'Animator', {
+
+        constructor: function ( beginValue, finishValue, setter ) {
+            if(arguments.length == 1) {
+                var opt = arguments[0];
+                this.beginVal = opt.beginValue;
+                this.finishVal = opt.finishValue;
+                this.setter = opt.setter;
+            } else {
+                this.beginVal = beginValue;
+                this.finishVal = finishValue;
+                this.setter = setter;
+            }
+        },
+
+        start: function ( target, duration, easing, delay, callback ) {
+            if ( arguments.length === 4 && typeof ( delay ) == 'function' ) {
+                callback = delay;
+                delay = 0;
+            }
+            var timeline = this.create( target, duration, easing, callback );
+            delay = parseTime( delay );
+            if ( delay > 0 ) {
+                setTimeout( function() {
+                    timeline.play();
+                }, delay );
+            } else {
+                timeline.play();
+            }
+            return timeline;
+        },
+
+        create: function ( target, duration, easing, callback ) {
+            var timeline;
+
+            duration = duration && parseTime( duration ) || Animator.DEFAULT_DURATION;
+            easing = easing || Animator.DEFAULT_EASING;
+
+            timeline = new Timeline( this, target, duration, easing );
+
+            if ( typeof ( callback ) == 'function' ) {
+                timeline.on( 'finish', callback );
+            }
+            return timeline;
+        }
+
+    } );
+
+    Animator.DEFAULT_DURATION = 300;
+    Animator.DEFAULT_EASING = 'linear';
+
+    var Shape = require( 'graphic/shape' );
+    require('core/class').extendClass( Shape, {
+        animate: function( animator, duration, easing, delay, callback ) {
+            var queue = this._KityAnimateQueue = this._KityAnimateQueue || [];
+            var timeline = animator.create(this, duration, easing, callback );
+
+            function dequeue() {
+                queue.shift();
+                if(queue.length) {
+                    setTimeout(queue[0].t.play.bind(queue[0].t), queue[0].d);
+                }
+            }
+            timeline.on('finish', dequeue);
+            queue.push( { t: timeline, d: delay } );
+            if(queue.length == 1) {
+                setTimeout(timeline.play.bind(timeline), delay);
+            }
+            return this;
+        },
+        stop: function() {
+            var queue = this._KityAnimateQueue;
+            if(queue) {
+                while(queue.length) {
+                    queue.shift().stop();
+                }
+            }
+        }
+    });
+
+    return Animator;
+} );
---
+++ src/animate/timeline.js
@@ -0,0 +1,229 @@
+define( function ( require, exports, module ) {
+
+    var easingTable = require( 'animate/easing' );
+    var Color = require( 'graphic/color' );
+    var Matrix = require( 'graphic/matrix' );
+    var EventHandler = require( 'core/eventhandler' );
+
+
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+
+    var globalFrameAction = [];
+
+    function execGlobalFrameAction() {
+        var pending = globalFrameAction;
+        globalFrameAction = [];
+        while(pending.length) {
+            pending.shift()();
+        }
+        requestAnimationFrame(execGlobalFrameAction);
+    }
+
+    execGlobalFrameAction();
+
+
+    // 不会深度遍历
+    function getPercentValue( b, f, p ) {
+        if ( typeof ( b ) == 'number' ) {
+            return b + ( f - b ) * p;
+        }
+        var value = {};
+        for ( var n in b ) {
+            if ( b.hasOwnProperty( n ) ) {
+                value[ n ] = b[ n ] + ( f[ n ] - b[ n ] ) * p;
+            }
+        }
+        return value;
+    }
+
+    function TimelineEvent( timeline, type, param ) {
+        this.timeline = timeline;
+        this.target = timeline.target;
+        this.type = type;
+        for(var name in param) {
+            if(param.hasOwnProperty(name)) {
+                this[name] = param[name];
+            }
+        }
+    }
+
+    var Timeline = require( 'core/class' ).createClass( 'Timeline', {
+        mixins: [EventHandler],
+        constructor: function ( animator, target, duration, easing ) {
+            this.time = 0;
+            this.duration = duration;
+            this.target = target;
+            this.easing = easingTable[ easing ];
+            this.status = 'ready';
+            this.animator = animator;
+            this.beginVal = animator.beginVal;
+            this.finishVal = animator.finishVal;
+            this.setter = animator.setter;
+        },
+        guessValueType: function () {
+            var value = this.beginVal;
+            if ( parseFloat( value ) ) {
+                this.valueType = 'number';
+                return;
+            }
+            // string as color
+            if ( typeof ( value ) == 'string' || value instanceof Color ) {
+                this.valueType = 'color';
+                return;
+            }
+            if ( value.x && value.y ) {
+                this.valueType = 'point';
+                return;
+            }
+            if ( value instanceof Matrix ) {
+                this.valueType = 'matrix';
+            }
+        },
+        nextFrame: function () {
+            if ( this.status != 'playing' ) {
+                return;
+            }
+
+            var ts = +new Date(),
+                lts = this.lastFrameTS || 0,
+                elapsed = ts - lts;
+            var target = this.target,
+                setter = this.setter;
+
+            // 
+            // 1. 首次播放 lts 为 0，则修正 elapsed 为一帧的长度
+            // 
+            // 2. 浏览器最小化或切换标签，requestAnimationFrame 不会执行。
+            //    检测时间超过 200 ms（频率小于 5Hz ） 判定为计时器暂停，重置为一帧长度
+            //    
+            //    ref: http://stackoverflow.com/questions/13133434/requestanimationframe-detect-stop
+            if ( elapsed > 200 ) {
+                elapsed = 1000 / 60;
+            }
+
+            this.time += elapsed;
+            this.setValue(this.getValue());
+
+            this.lastFrameTS = ts;
+
+            if ( this.time >= this.duration ) {
+                this.timeUp();
+            }
+
+            globalFrameAction.push(this.nextFrame.bind(this));
+        },
+        getPlayTime: function() {
+            return this.rollbacking ? this.duration - this.time : this.time;
+        },
+        getValue: function () {
+            var b = this.beginVal,
+                f = this.finishVal,
+                p = this.easing( this.getPlayTime(), 0, 1, this.duration ),
+                v;
+
+            switch ( this.valueType ) {
+                case 'color':
+                    b = b.getValues();
+                    f = f.getValues();
+                    v = getPercentValue( b, f, p );
+                    return Color.createRGBA( v.r, v.g, v.b, v.a );
+                case 'matrix':
+                    b = b.getMatrix();
+                    f = f.getMatrix();
+                    v = getPercentValue( b, f, p );
+                    return new Matrix( v );
+                default:
+                    return getPercentValue( b, f, p );
+            }
+        },
+        setValue: function( value ) {
+            this.setter.call( this.target, this.target, value );
+        },
+        play: function () {
+            var ctx = this.context;
+            var me = this;
+            var lastStatus = this.status;
+            this.status = 'playing';
+            switch ( lastStatus ) {
+                case 'ready':
+                    this.beginVal = typeof ( this.beginVal ) == 'function' ?
+                        this.beginVal.call( this.target, this.target ) :
+                        this.beginVal;
+                    this.finishVal = typeof ( this.finishVal ) == 'function' ?
+                        this.finishVal.call( this.target, this.target ) :
+                        this.finishVal;
+                    this.time = 0;
+                    this.guessValueType();
+                    this.nextFrame();
+                    break;
+                case 'finished':
+                case 'stoped':
+                    this.time = 0;
+                    this.nextFrame();
+                    break;
+                case 'paused':
+                    this.lastFrameTS = 0;
+                    this.nextFrame();
+            }
+            this.fire('play', new TimelineEvent(this, 'play', {
+                lastStatus: lastStatus
+            }));
+            return this;
+        },
+        pause: function () {
+            this.status = 'paused';
+            this.fire('pause', new TimelineEvent(this, 'pause'));
+            return this;
+        },
+        stop: function () {
+            this.status = 'stoped';
+            this.setValue(this.finishVal);
+            this.rollbacking = false;
+            this.fire('stop', new TimelineEvent(this, 'stop'));
+            return this;
+        },
+        timeUp: function () {
+            if ( this.repeatOption ) {
+                this.time = 0;
+                if ( this.rollback ) {
+                    if ( this.rollbacking ) {
+                        this.decreaseRepeat();
+                        this.rollbacking = false;
+                    } else {
+                        this.rollbacking = true;
+                        this.fire('rollback', new TimelineEvent(this, 'rollback'));
+                    }
+                } else {
+                    this.decreaseRepeat();
+                }
+                if ( !this.repeatOption ) {
+                    this.finish();
+                } else {
+                    this.fire('repeat', new TimelineEvent(this, 'repeat'));
+                }
+            } else {
+                this.finish();
+            }
+        },
+        finish: function() {
+            this.setValue(this.finishVal);
+            this.status = 'finished';
+            this.fire('finish', new TimelineEvent(this, 'finish'));
+        },
+        decreaseRepeat: function () {
+            if ( this.repeatOption !== true ) {
+                this.repeatOption--;
+            }
+        },
+        repeat: function ( repeat, rollback ) {
+            this.repeatOption = repeat;
+            this.rollback = rollback;
+            return this;
+        }
+    } );
+
+    return Timeline;
+} );
