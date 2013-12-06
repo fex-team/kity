7c65efe09de0a95ac7f9e11b67f19021ac362c75
techird <techird@qq.com>
2013-12-06 12:49:10 +0800

    对 Use 的图形进行 fill 和 stroke 进行属性删除

--- src/graphic/use.js
+++ src/graphic/use.js
@@ -5,8 +5,9 @@
 define( function ( require, exports, module ) {
 
     var Svg = require( "graphic/svg" );
+    var Class = require( "core/class" );
 
-    return require( "core/class" ).createClass( 'Use', {
+    var Use = Class.createClass( 'Use', {
 
         base: require( "graphic/shape" ),
 
@@ -19,11 +20,30 @@ define( function ( require, exports, module ) {
             shapeId = shape.getId();
 
             if ( shapeId ) {
-                this.node.setAttributeNS( Svg.xlink,'xlink:href', '#' + shapeId );
+                this.node.setAttributeNS( Svg.xlink, 'xlink:href', '#' + shapeId );
+            }
+
+            // by techird
+            // 作为 Use 的图形，如果没有 fill 和 stroke，移除默认的 'none' 值，用于 Use 覆盖
+            if ( shape.node.getAttribute( 'fill' ) === 'none' ) {
+                shape.node.removeAttribute( 'fill' );
+            }
+            if ( shape.node.getAttribute( 'stroke' ) === 'none' ) {
+                shape.node.removeAttribute( 'stroke' );
             }
 
         }
 
     } );
+
+    var Shape = require( 'graphic/shape' );
+    Class.extendClass( Shape, {
+        // fast-use
+        use: function () {
+            return new Use(this);
+        }
+    } );
+
+    return Use;
 
 } );
