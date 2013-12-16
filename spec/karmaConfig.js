016f627bc92a1605438699328db805cd452f5e7d
techird <techird@qq.com>
2013-12-16 13:09:45 +0800

    merge

---
+++ spec/karmaConfig.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: dongyancen
+ * Date: 13-12-10
+ * Time: 下午7:24
+ * To change this template use File | Settings | File Templates.
+ */
+seajs.config( {
+    base: 'http://localhost:8089/kitygraph_git/src/'
+} );
+
+//seajs.config( {
+//    base: 'http://localhost/kitygraph_b/src/'
+//} );
