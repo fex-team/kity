7a493298df0fe899d403b33ba76550a0ee608610
techird <techird@qq.com>
2013-12-25 16:50:13 +0800

    chinamap demo

--- src/graphic/paper.js
+++ src/graphic/paper.js
@@ -1,105 +1,81 @@
-define( function ( require, exports, module ) {
-    var Class = require( 'core/class' );
-    var utils = require( 'core/utils' );
-    var svg = require( 'graphic/svg' );
-    var Container = require( 'graphic/container' );
-    var ShapeContainer = require( 'graphic/shapecontainer' );
-    var EventHandler = require( 'graphic/eventhandler' );
-    var Styled = require( 'graphic/styled' );
-    var Matrix = require( 'graphic/matrix' );
+define(function(require, exports, module) {
+    var Class = require('core/class');
+    var utils = require('core/utils');
+    var svg = require('graphic/svg');
+    var Container = require('graphic/container');
+    var ShapeContainer = require('graphic/shapecontainer');
+    var ViewBox = require('graphic/viewbox');
+    var EventHandler = require('graphic/eventhandler');
+    var Styled = require('graphic/styled');
+    var Matrix = require('graphic/matrix');
 
-    var Paper = Class.createClass( 'Paper', {
+    var Paper = Class.createClass('Paper', {
 
-        mixins: [ ShapeContainer, EventHandler, Styled ],
+        mixins: [ShapeContainer, EventHandler, Styled, ViewBox],
 
-        constructor: function ( container ) {
+        constructor: function(container) {
             this.callBase();
 
             this.node = this.createSVGNode();
             this.node.paper = this;
 
-            this.node.appendChild( this.resourceNode = svg.createNode( 'defs' ) );
-            this.node.appendChild( this.shapeNode = svg.createNode( 'g' ) );
+            this.node.appendChild(this.resourceNode = svg.createNode('defs'));
+            this.node.appendChild(this.shapeNode = svg.createNode('g'));
 
             this.resources = new Container();
-            this.setWidth( '100%' ).setHeight( '100%' );
+            this.setWidth('100%').setHeight('100%');
 
-            if ( container ) {
-                this.renderTo( container );
+            if (container) {
+                this.renderTo(container);
             }
             this.callMixin();
         },
 
-        renderTo: function ( container ) {
-            if ( utils.isString( container ) ) {
-                container = document.getElementById( container );
+        renderTo: function(container) {
+            if (utils.isString(container)) {
+                container = document.getElementById(container);
             }
             this.container = container;
-            container.appendChild( this.node );
+            container.appendChild(this.node);
         },
 
-        createSVGNode: function () {
-            var node = svg.createNode( 'svg' );
-            node.setAttribute( "xmlns", "http://www.w3.org/2000/svg" );
-            node.setAttribute( "xmlns:xlink", "http://www.w3.org/1999/xlink" );
+        createSVGNode: function() {
+            var node = svg.createNode('svg');
+            node.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+            node.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
             return node;
         },
 
-        getNode: function () {
+        getNode: function() {
             return this.node;
         },
 
-        getContainer: function () {
+        getContainer: function() {
             return this.container;
         },
 
-        getWidth: function () {
-            return +this.node.getAttribute( 'width' );
+        getWidth: function() {
+            return +this.node.getAttribute('width');
         },
 
-        setWidth: function ( width ) {
-            this.node.setAttribute( 'width', width );
+        setWidth: function(width) {
+            this.node.setAttribute('width', width);
             return this;
         },
 
-        getHeight: function () {
-            return +this.node.getAttribute( 'height' );
+        getHeight: function() {
+            return +this.node.getAttribute('height');
         },
 
-        setHeight: function ( height ) {
-            this.node.setAttribute( 'height', height );
+        setHeight: function(height) {
+            this.node.setAttribute('height', height);
             return this;
         },
 
-        getViewBox: function () {
-            var attr = this.node.getAttribute( 'viewBox' );
-            if ( attr === null ) {
-                return {
-                    x: 0,
-                    y: 0,
-                    width: this.node.clientWidth,
-                    height: this.node.clientHeight
-                };
-            } else {
-                attr = attr.split( ' ' );
-                return {
-                    x: +attr[ 0 ],
-                    y: +attr[ 1 ],
-                    width: +attr[ 2 ],
-                    height: +attr[ 3 ]
-                };
-            }
-        },
-
-        setViewBox: function ( x, y, width, height ) {
-            this.node.setAttribute( 'viewBox', [ x, y, width, height ].join( ' ' ) );
-            return this;
-        },
-
-        setViewPort: function ( cx, cy, zoom ) {
+        setViewPort: function(cx, cy, zoom) {
             var viewport, box;
-            if ( arguments.length == 1 ) {
-                viewport = arguments[ 0 ];
+            if (arguments.length == 1) {
+                viewport = arguments[0];
                 cx = viewport.center.x;
                 cy = viewport.center.y;
                 zoom = viewport.zoom;
@@ -114,18 +90,18 @@ define( function ( require, exports, module ) {
                 zoom: zoom
             };
             var matrix = new Matrix();
-            var dx = ( box.x + box.width / 2 ) - cx,
-                dy = ( box.y + box.height / 2 ) - cy;
-            matrix.translate( -cx, -cy );
-            matrix.scale( zoom );
-            matrix.translate( cx, cy );
-            matrix.translate( dx, dy );
-            this.shapeNode.setAttribute( 'transform', matrix );
+            var dx = (box.x + box.width / 2) - cx,
+                dy = (box.y + box.height / 2) - cy;
+            matrix.translate(-cx, -cy);
+            matrix.scale(zoom);
+            matrix.translate(cx, cy);
+            matrix.translate(dx, dy);
+            this.shapeNode.setAttribute('transform', matrix);
             return this;
         },
 
-        getViewPort: function () {
-            if ( !this.viewport ) {
+        getViewPort: function() {
+            if (!this.viewport) {
                 var box = this.getViewBox();
                 this.viewport = {
                     zoom: 1,
@@ -138,40 +114,40 @@ define( function ( require, exports, module ) {
             return this.viewport;
         },
 
-        addResource: function ( resource ) {
-            this.resources.appendItem( resource );
-            if ( resource.node ) {
-                this.resourceNode.appendChild( resource.node );
+        addResource: function(resource) {
+            this.resources.appendItem(resource);
+            if (resource.node) {
+                this.resourceNode.appendChild(resource.node);
             }
 
 
             return this;
         },
 
-        removeResource: function ( resource ) {
-            if ( resource.remove ) {
+        removeResource: function(resource) {
+            if (resource.remove) {
                 resource.remove();
             }
-            if ( resource.node ) {
-                this.resourceNode.removeChild( resource.node );
+            if (resource.node) {
+                this.resourceNode.removeChild(resource.node);
             }
             return this;
         },
 
-        getPaper: function () {
+        getPaper: function() {
             return this;
         }
-    } );
+    });
 
-    var Shape = require( 'graphic/shape' );
-    Class.extendClass( Shape, {
-        getPaper: function () {
+    var Shape = require('graphic/shape');
+    Class.extendClass(Shape, {
+        getPaper: function() {
             var parent = this.container;
-            while ( parent && parent instanceof Paper === false ) {
+            while (parent && parent instanceof Paper === false) {
                 parent = parent.container;
             }
             return parent;
         }
-    } );
+    });
     return Paper;
-} );
+});
