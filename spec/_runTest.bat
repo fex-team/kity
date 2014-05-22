5f8e8c2ef823601ed50618a40a9b8a8e7bc16c6b
yancend <dongyancen@baidu.com>
2014-05-22 15:52:22 +0800

    [test]添加在ci上运行的文件

---
+++ spec/_runTest.bat
@@ -0,0 +1,7 @@
+setlocal EnableDelayedExpansion
+del /q /s .\spec\coverage\json_files\*.json
+rd /s /q .\coverage
+for /f %%p in ('grunt ') do set gruntRe=%%p
+for /f %%j in ('karma start ./spec/karma.conf.js') do set karmaRe=%%j
+for /f  %%i in ('node ./spec/_mergeCoverageForIstanbul.js') do set re=%%i
+istanbul report html spec/coverage/json_files/%re%.json
